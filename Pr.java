13338b3dca59760a1cf9808e591844d8b8286590
set3 <22h51a05p6@cmrcet.ac.in>
2024-12-08 18:54:50 +0530

    commiting

---
+++ Pr.java
@@ -0,0 +1,19 @@
+public class Pr {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 100; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        for (int j = 2; j <= Math.sqrt(num); j++) {
+            if (num % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
